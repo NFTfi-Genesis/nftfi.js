9392f201be9a60b9bff8dbe910606be8d44d25fa
b4nd1do <b4nd1do.yo@gmail.com>
2022-11-09 13:54:40 +0200

    Latest updates

--- src/nftfi/config.js
+++ src/nftfi/config.js
@@ -112,6 +112,7 @@ mainnetConfig.loan.fixed.v2.abi = [
   'function liquidateOverdueLoan(uint32 _loanId) nonpayable returns()',
   'function payBackLoan(uint32 _loanId)',
   'function acceptOffer(tuple(uint256 loanPrincipalAmount, uint256 maximumRepaymentAmount, uint256 nftCollateralId, address nftCollateralContract, uint32 loanDuration, uint16 loanAdminFeeInBasisPoints, address loanERC20Denomination, address referrer) _offer, tuple(uint256 nonce, uint256 expiry, address signer, bytes signature) _signature, tuple(address revenueSharePartner, uint16 referralFeeInBasisPoints) _borrowerSettings)',
+  'function getWhetherNonceHasBeenUsedForUser(address _user, uint256 _nonce) view returns (bool)',
   'event LoanStarted(uint32 indexed loanId, address indexed borrower, address indexed lender, tuple(uint256 loanPrincipalAmount, uint256 maximumRepaymentAmount, uint256 nftCollateralId, address loanERC20Denomination, uint32 loanDuration, uint16 loanInterestRateForDurationInBasisPoints, uint16 loanAdminFeeInBasisPoints, address nftCollateralWrapper, uint64 loanStartTime, address nftCollateralContract, address borrower) loanTerms, tuple(address revenueSharePartner, uint16 revenueShareInBasisPoints, uint16 referralFeeInBasisPoints) loanExtras)'
 ];
 mainnetConfig.loan.fixed.v2_1.name = 'v2-1.loan.fixed';
